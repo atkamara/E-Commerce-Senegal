c3ffdb5cdb46a467b7fa64c16061d09f2b3742a7
atkamara <atkamara.ds@outlook.com>
2024-05-07 10:58:33 +0000

    docstring

--- src/falcon/Con.py
+++ src/falcon/Con.py
@@ -1,98 +1,93 @@
-"""
-Provides a Redis connection interface.
-This module defines the Redis class, which serves as a connection interface 
-for interacting with a Redis database.
+"""Con.py - Module for Redis Cursor Objects
+This module provides Cursor objects for interacting with Redis database.
 Classes:
-    Redis: Represents a connection to a Redis database.
-Functions:
-    flatten_dict: Flatten a dictionary with nested dictionaries and lists.
+    Redis: Cursor object for Redis operations.
+    RedisUser: Cursor object for Redis operations specific to users.
+Usage:
+    Typical usage involves creating an instance of Redis or RedisUser and using its methods for database operations.
+Example:
+    # Import Con module
+    from .Con import Redis, RedisUser
+    # Create Redis instance
+    redis_instance = Redis(domain="example", category="test")
+    # Get pattern
+    pattern = redis_instance.pattern
+    # Increment value
+    incr_value = redis_instance.incr
+    # Get id
+    id_value = redis_instance.id
+    # Push data
+    result_data = {"key1": "value1", "key2": "value2"}
+    redis_instance.push(result_data)
+    # Create RedisUser instance
+    redis_user_instance = RedisUser(domain="example", category="test")
+    # Get users pattern
+    users_pattern = redis_user_instance.users_pattern("1234567890")
+    # Push data for user
+    result_data_user = {"Contact": "1234567890", "ProductTitle": "Example Product"}
+    redis_user_instance.push(result_data_user)
+    # Set parent
+    redis_user_instance.set_parent("1234567890", "parent")
+    # Get tel parent
+    tel_parent = redis_user_instance.get_tel_parent("1234567890")
+    # Update tel parent
+    updated_tel_parent = redis_user_instance.update_tel_parent("1234567890", tel_parent, "new_parent")
+    # Get tel id
+    tel_id = redis_user_instance.tel_id("1234567890")
 """
 import redis
 from .Model import Cursor
 from .utils import flatten_dict
 class Redis(Cursor):
-    """
-    Represents a connection to a Redis database.
-    Attributes:
-        category (str): The category for the Redis data.
-        partition (str): The partition for the Redis data.
-        engine: The Redis connection object.
-    Methods:
-        __init__: Initializes a Redis object with the provided parameters.
-        push: Pushes a mapped data object to the Redis database.
-    """
+    """Cursor object for Redis operations."""
     def __init__(self,
-                 domain:str,
-                 category : str,
-                 partition: str='',
-                 root='root',
+                 domain: str,
+                 category: str,
+                 partition: str = '',
+                 root: str = 'root',
                  **kwargs) -> None:
         """
-        Initializes a Redis object with the provided parameters.
+        Initialize Redis Cursor.
         Args:
-            category (str): The category for the Redis data.
-            partition (str): The partition for the Redis data.
-            host (str): The hostname or IP address of the Redis server.
-            port (int, optional): The port number of the Redis server. Defaults to 6379.
-            username (str, optional): The username for Redis authentication. Defaults to None.
-            password (str, optional): The password for Redis authentication. Defaults to None.
-            **kwargs: Additional keyword arguments passed to the Redis connection.
-        Returns:
-            None
+            domain (str): Domain value.
+            category (str): Category value.
+            partition (str, optional): Partition value. Defaults to ''.
+            root (str, optional): Root value. Defaults to 'root'.
+            **kwargs: Additional keyword arguments for redis.Redis.
+        Attributes:
+            root (str): Root value.
+            domain (str): Domain value.
+            category (str): Category value.
+            partition (str): Partition value.
+            engine: Redis engine.
         """
-        self.root=root
+        self.root = root
         self.domain = domain
         self.category = category
         self.partition = partition
         self.engine = redis.Redis(**kwargs)
     @property
     def pattern(self):
-        """
-        Generates a pattern based on the domain, category, and partition attributes.
-        
-        Returns:
-            str: The generated pattern, where non-empty attributes are joined with a colon (':').
-        """
-        return ':'.join(folder for folder in [self.root,self.domain, self.category, self.partition] if folder)
+        """Get pattern."""
+        return ':'.join(folder for folder in [self.root, self.domain, self.category, self.partition] if folder)
     @property
     def incr(self):
-        """
-        Computes the increment value associated with the category using the engine's 'incr' method.
-        
-        Returns:
-            int: The increment value.
-        """
+        """Get increment value."""
         return self.engine.incr('items')
     @property
     def id(self):
-        """
-        Generates an ID based on the pattern and increment values.
-        
-        Returns:
-            str: The generated ID, formatted as 'pattern:incr'.
-        """
+        """Get id."""
         return f'{self.pattern}:{self.incr}'
     def pipe(self, data):
-        """
-        Processes the input data and returns a flattened string representation.
-        
-        Args:
-            data: Input data to be processed.
-        Returns:
-            str: Flattened string representation of the input data.
-        """
+        """Pipe data."""
         return flatten_dict(data.to_dict())
-    def push(self,result):
-        """
-        Pushes a mapped data object to the Redis database.
-        Args:
-            result: The mapped data object to push to Redis.
-        Returns:
-            None
-        """
-        self.engine.hset(self.id,mapping=self.pipe(result))
+    def push(self, result):
+        """Push data."""
+        self.engine.hset(self.id, mapping=self.pipe(result))
 class RedisUser(Redis):
-    def users_pattern(self,tel):
+    """Cursor object for Redis operations specific to users."""
+    def users_pattern(self, tel):
+        """Get users pattern."""
         return ':'.join([
             self.root,
             'Users',
@@ -100,35 +95,42 @@ class RedisUser(Redis):
             'Domains',
             self.domain,
             'Categories',
-            self.category])
-    def push(self,result):
+            self.category
+        ])
+    def push(self, result):
+        """Push data for user."""
         piped = self.pipe(result)
         tel_id = self.tel_id(piped['Contact'])
         title = piped['ProductTitle']
         pattern = self.users_pattern(tel_id)
         incr = self.engine.incr(pattern)
         id = f'{pattern}:{title}:{incr}'
-        self.engine.hset(id,mapping=piped)
-    def parent_tel_pattern(self,tel,parent):
+        self.engine.hset(id, mapping=piped)
+    def parent_tel_pattern(self, tel, parent):
+        """Get parent tel pattern."""
         return f'{self.root}:Ids:{parent}:{tel}'
-    def set_parent(self,tel,parent):
-        pattern = self.parent_tel_pattern(tel,parent)
-        self.engine.set(pattern,tel)
+    def set_parent(self, tel, parent):
+        """Set parent."""
+        pattern = self.parent_tel_pattern(tel, parent)
+        self.engine.set(pattern, tel)
         return pattern
-    def get_tel_parent(self,tel):
+    def get_tel_parent(self, tel):
+        """Get tel parent."""
         old = self.engine.keys(pattern=f'{self.root}:Ids:*:{tel}')
         if not old:
-            old = [self.set_parent(tel,tel)]
+            old = [self.set_parent(tel, tel)]
         return old
-    def update_tel_parent(self,tel,old_parent,parent):
+    def update_tel_parent(self, tel, old_parent, parent):
+        """Update tel parent."""
         self.engine.delete(old_parent[0])
-        return self.set_parent(tel,parent)
-    def tel_id(self,tel):
+        return self.set_parent(tel, parent)
+    def tel_id(self, tel):
+        """Get tel id."""
         tels = tel.split(';')
         tel = 'Unknown'
         if tels[0]:
-            parents = [self.get_tel_parent(tel) for tel in tels ]
+            parents = [self.get_tel_parent(tel) for tel in tels]
             tel = parents.pop()[0].split(':')[2]
-            for other,old_parent in zip(tels,parents):
-                self.update_tel_parent(other,old_parent,tel)
+            for other, old_parent in zip(tels, parents):
+                self.update_tel_parent(other, old_parent, tel)
         return tel
